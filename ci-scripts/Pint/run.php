eb2644c0ad7e4b97a2572c640a3df1030959473b
Oscar Cabrera <o.cabrera@cyberpuerta.mx>
2024-08-06 04:34:51 -0600

    move CIScripts to AnalyzerTool

--- ci-scripts/Pint/run.php
+++ ci-scripts/Pint/run.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-namespace Oscabrera\ModelRepository\CIScripts;
+namespace Oscabrera\AnalyzerTool\CIScripts;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-use Oscabrera\ModelRepository\CIScripts\Pint\PintAnalyzer as Analyzer;
+use Oscabrera\AnalyzerTool\CIScripts\Pint\PintAnalyzer as Analyzer;
 
 exit((new Analyzer(array_slice($_SERVER['argv'], 1)))->analyze() ? 0 : 1);
